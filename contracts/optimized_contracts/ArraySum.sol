c04bb71bacb1e5a6e7c8dd185e53c3d5e34ed7f7
asusrid <alexsurid@gmail.com>
2024-05-31 19:21:58 +0200

    solving arraysum: adding an if stat

---
+++ contracts/optimized_contracts/ArraySum.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.15;
+
+contract OptimizedArraySum {
+    // Do not modify this
+    uint256[] array;
+
+    // Do not modify this
+    function setArray(uint256[] memory _array) external {
+        require(_array.length <= 10, 'too long');
+        array = _array;
+    }
+
+    // optimize this function
+    function getArraySum() external view returns (uint256) {
+        if (array.length == 0) return 0;
+        uint256 sum = 0;
+        uint256 len = array.length;
+        for (uint256 i = 0; i < len; i++) {
+            sum += array[i];
+        }
+        return sum;
+    }
+}
